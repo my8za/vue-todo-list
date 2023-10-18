97fc5a9dad66632a7fe0f0b43e2c6fef1abfd896
my8za <emilie15@naver.com>
2023-10-18 14:37:44 +0900

    add components

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,9 @@
 
 <script>
 import TheHeading from "./components/TheHeading.vue";
-import TheFooter from "./components/TheFooter.vue";
 import TodoListInput from "./components/TodoListInput.vue";
 import TodoListItem from "./components/TodoListItem.vue";
+import TheFooter from "./components/TheFooter.vue";
 
 export default {
   components: {
@@ -22,3 +22,17 @@ export default {
   },
 };
 </script>
+
+<style>
+body {
+  background: #f6f6f6;
+  text-align: center;
+}
+input,
+button {
+  border-style: groove;
+}
+.shadow {
+  box-shadow: 5px 10px 5px rgba(0, 0, 0, 0.03);
+}
+</style>
--- src/components/TodoListInput.vue
+++ src/components/TodoListInput.vue
@@ -1,6 +1,60 @@
 <template>
-  <form>
-    <input type="text" />
-    <button>+</button>
+  <form @submit.prevent="handleAddTodo" class="input-box shadow">
+    <input type="text" v-model="newTodoItem" />
+    <button class="btn-add">+</button>
   </form>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTodoItem: "",
+    };
+  },
+  methods: {
+    clearInput() {
+      this.newTodoItem = "";
+    },
+    handleAddTodo() {
+      if (this.newTodoItem !== "") {
+        let todo = { content: this.newTodoItem, done: false };
+        localStorage.setItem(this.newTodoItem, JSON.stringify(todo));
+        this.clearInput();
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.input-box {
+  position: relative;
+  height: 50px;
+  background: #fff;
+  border-radius: 5px;
+  line-height: 50px;
+}
+.input-box > input {
+  width: 100%;
+  padding-left: 20px;
+  border-style: none;
+  outline: none;
+  font-size: 0.9rem;
+}
+.btn-add {
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 3rem;
+  height: 50px;
+  border: none;
+  border-radius: 0 5px 5px 0;
+  background: linear-gradient(to right, #6478fb, #8763fb);
+  vertical-align: middle;
+  font-size: 1.7rem;
+  font-weight: 700;
+  color: #fff;
+  cursor: pointer;
+}
+</style>
